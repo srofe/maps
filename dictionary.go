0dc6fe01434e35fe18012ab45913f6b5838d5399
Simon Rofe <simon.rofe@ieee.org>
2022-01-12 15:52:29 +1000

    Report an error if the element doesn't exist

--- dictionary.go
+++ dictionary.go
@@ -1,7 +1,13 @@
 package main
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (dictionary Dictionary) Search(word string) string {
-	return dictionary[word]
+func (dictionary Dictionary) Search(word string) (string, error) {
+	value, ok := dictionary[word]
+	if !ok {
+		return "", errors.New("could not find the word you are looking for")
+	}
+	return value, nil
 }
--- dictionary_test.go
+++ dictionary_test.go
@@ -6,9 +6,18 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("A Dictionary shall have a Search method to find elements it contains.", func(t *testing.T) {
-		got := dictionary.Search("test")
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 		assertStrings(t, got, want)
+	})
+
+	t.Run("The Search method shall return an error if the element is not contained in the map.", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you are looking for"
+		if err == nil {
+			t.Fatal("Expected to get an error.")
+		}
+		assertStrings(t, err.Error(), want)
 	})
 }
 
