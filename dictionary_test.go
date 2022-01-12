57d1a2c2c6acfbab4fe855547d0e1919840297d4
Simon Rofe <simon.rofe@ieee.org>
2022-01-13 07:09:01 +1000

    Refactor to use sub-tests

--- dictionary_test.go
+++ dictionary_test.go
@@ -22,11 +22,13 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+	t.Run("A Dictionary shall allow words and definitions to be added.", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
+		dictionary.Add(word, definition)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
