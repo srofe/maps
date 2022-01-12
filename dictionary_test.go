2ed88dd2c67f8553b4958f55d4c97db4e2b45022
Simon Rofe <simon.rofe@ieee.org>
2022-01-12 15:44:28 +1000

    Refactor to use sub-tests

--- dictionary_test.go
+++ dictionary_test.go
@@ -4,9 +4,12 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
-	got := dictionary.Search("test")
-	want := "this is just a test"
-	assertStrings(t, got, want)
+
+	t.Run("A Dictionary shall have a Search method to find elements it contains.", func(t *testing.T) {
+		got := dictionary.Search("test")
+		want := "this is just a test"
+		assertStrings(t, got, want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
