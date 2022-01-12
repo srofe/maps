80bf9b668a688bd16b731ffdfd35559c2cd747de
Simon Rofe <simon.rofe@ieee.org>
2022-01-13 07:25:46 +1000

    Return an error if a word already in Dictionary

--- dictionary.go
+++ dictionary.go
@@ -4,14 +4,28 @@ import "errors"
 
 type Dictionary map[string]string
 
+var (
+	ErrNotFound   = errors.New("could not find the word you are looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
 func (dictionary Dictionary) Search(word string) (string, error) {
 	definition, ok := dictionary[word]
 	if !ok {
-		return "", errors.New("could not find the word you are looking for")
+		return "", ErrNotFound
 	}
 	return definition, nil
 }
 
-func (dictionary Dictionary) Add(word, definition string) {
-	dictionary[word] = definition
+func (dictionary Dictionary) Add(word, definition string) error {
+	_, err := dictionary.Search(word)
+	switch err {
+	case ErrNotFound:
+		dictionary[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
 }
--- dictionary_test.go
+++ dictionary_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
@@ -26,7 +28,17 @@ func TestAdd(t *testing.T) {
 		dictionary := Dictionary{}
 		word := "test"
 		definition := "this is just a test"
-		dictionary.Add(word, definition)
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("A Dictionary shall return an error when adding a word that already exists in the Dictionary.", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+		assertError(t, err, ErrWordExists)
 		assertDefinition(t, dictionary, word, definition)
 	})
 }
@@ -46,5 +58,12 @@ func assertDefinition(t testing.TB, dictionary Dictionary, word, definition stri
 	}
 	if definition != got {
 		t.Errorf("got %q want %q", got, definition)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
