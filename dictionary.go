776ed93af12b65188ea6134cb5903814a5950ec0
Simon Rofe <simon.rofe@ieee.org>
2022-01-12 15:59:15 +1000

    Rename returned variable
    The Dictionary will contain the definition for words. The words are the
    keys, and the definitions are the values.

--- dictionary.go
+++ dictionary.go
@@ -5,9 +5,9 @@ import "errors"
 type Dictionary map[string]string
 
 func (dictionary Dictionary) Search(word string) (string, error) {
-	value, ok := dictionary[word]
+	definition, ok := dictionary[word]
 	if !ok {
 		return "", errors.New("could not find the word you are looking for")
 	}
-	return value, nil
+	return definition, nil
 }
